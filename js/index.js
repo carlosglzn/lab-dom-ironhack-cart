2083dff7d084d9bdb159a6db04db22d4c2b916c0
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-06-28 18:36:22 -0500

    All done

--- js/index.js
+++ js/index.js
@@ -40,18 +40,77 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const targetRow = target.parentNode.parentNode
+  const tBody = targetRow.parentNode
+
+  tBody.removeChild(targetRow)
+
+  calculateAll()
 }
 
 // ITERATION 5
 
-function createProduct() {
-  //... your code goes here
+function createProduct(event) {
+
+  const tr = event.currentTarget.parentNode.parentNode;
+
+  //agarrar name, price
+
+  const name = tr.querySelector(".create-product input").value;
+  const price = tr.querySelector("input[type=number]").value;
+
+  console.log(name, price);
+
+  const newElement = document.createElement("tr");
+  newElement.className = "product";
+
+  let newInnerHTML = `<td class="name">
+  <span>${name}</span>
+</td>
+<td class="price">$<span>${Number(price).toFixed(2)}</span></td>
+<td class="quantity">
+  <input type="number" value="0" min="0" placeholder="Quantity" />
+</td>
+<td class="subtotal">$<span>0</span></td>
+<td class="action">
+  <button class="btn btn-remove">Remove</button>
+</td>`;
+
+newElement.innerHTML = newInnerHTML;
+newElement
+  .getElementsByClassName("btn-remove")[0]
+  .addEventListener("click", removeProduct)
+
+const body = document.getElementsByTagName("tbody");
+body[0].appendChild(newElement);
+
+clearNewProduct();
+
 }
+
+function clearNewProduct() {
+  
+  const createProduct = document.getElementsByTagName("tfoot")[0];
+  const inputs = createProduct.getElementsByTagName("input");
+  
+  inputs[0].value = null;
+  inputs[1].value = 0;
+}
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+
+  const removeBtns = document.getElementsByClassName("btn btn-remove");
+  for (const btn of removeBtns) {
+    btn.addEventListener("click", removeProduct);
+  }
+
+  const createBtn = document.getElementById("create");
+  createBtn.addEventListener("click", createProduct);
+
 });
